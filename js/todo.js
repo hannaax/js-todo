f33b8b9bfd201996922c756cfbef100f84893e6a
hannaax <hanan941008@gmail.com>
2023-04-01 23:28:29 +0900

    Feat: todo 생성 구현

--- js/todo.js
+++ js/todo.js
@@ -4,7 +4,17 @@ const todoList = document.getElementById("todo-list");
 
 const handleToDoSubmit = (e) => {
   e.preventDefault();
-  console.log(todoInput.value);
+  const newTodo = todoInput.value;
+  todoInput.value = "";
+  paintToDo(newTodo);
+};
+
+const paintToDo = (newTodo) => {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  todoList.appendChild(li);
 };
 
 todoForm.addEventListener("submit", handleToDoSubmit);
