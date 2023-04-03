9be0bc25a8f436d51e252b370027973e1f0c2fbd
hannaax <hanan941008@gmail.com>
2023-04-03 15:57:23 +0900

    Feat: localStorage에 todo 저장 구현

--- js/todo.js
+++ js/todo.js
@@ -2,19 +2,53 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
-const handleToDoSubmit = (e) => {
-  e.preventDefault();
-  const newTodo = todoInput.value;
-  todoInput.value = "";
-  paintToDo(newTodo);
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+const saveToDos = () => {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+};
+
+const deleteToDo = (e) => {
+  const li = e.target.parentElement;
+  li.remove();
 };
 
 const paintToDo = (newTodo) => {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
   li.appendChild(span);
-  span.innerText = newTodo;
+  li.appendChild(button);
   todoList.appendChild(li);
+  // 로컬스토리지 추가
+  // 불러와서 화면에 띄움
+};
+
+const handleToDoSubmit = (e) => {
+  e.preventDefault();
+  const newTodo = todoInput.value;
+  todoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
 };
 
 todoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
